b72f510a907e1b36c00aad0fc706c544dcb1d618
Lanck_vic <36233728+VictoryHero@users.noreply.github.com>
2018-11-10 11:31:48 +0800

    做了较大修改 增添了校庆元素

--- utils/config.js
+++ utils/config.js
@@ -1,26 +1,13 @@
-/*
- * 
- * WordPres版微信小程序
- * author: jianbo
- * organization: 守望轩  www.watch-life.net
- * github:    https://github.com/iamxjb/winxin-app-watch-life.net
- * 技术支持微信号：iamxjb
- * 开源协议：MIT
- * Copyright (c) 2017 https://www.watch-life.net All rights reserved.
- */
 
 
-
-//配置域名,域名只修改此处。
-//如果wordpress没有安装在网站根目录请加上目录路径,例如："www.watch-life.net/blog"
 var DOMAIN = "lzu.abaoli.cn/wordpress/wordpress";
-var MINAPPTYPE="1";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
-var WEBSITENAME="守望轩"; //网站名称
+var MINAPPTYPE="1";//小程序的类型
+var WEBSITENAME="兰州大学-校园展示作品"; //网站名称
 var ABOUTID = 1136; //wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
 var PAGECOUNT='10'; //每页文章数目
 var CATEGORIESID='all'  //专题页显示全部的分类
 //var CATEGORIESID = '1,1059,98,416,189,374,6,463';//指定专题页显示的分类的id
-var INDEXLISTTYPE="all" //首页显示所有分类
+var INDEXLISTTYPE="37,36" //首页显示所有分类
 //var INDEXLISTTYPE ="1" //指定首页显示分类的id
 var PAYTEMPPLATEID = 'hzKpxuPF2rw7O-qTElkeoE0lMwr0O4t9PJkLyt6v8rk';//赞赏消息模版id
 var REPLAYTEMPPLATEID = 'IiAVoBWP34u1uwt801rI_Crgen7Xl2lvAGP67ofJLo8';//回复评论消息模版id
@@ -30,7 +17,7 @@ var POSTERIMGURL ="../../images/logo700.png"; //生成海报如果没有首图�
 //设置downloadFile合法域名,不带https ,在中括号([])里增加域名，格式：{id=**,domain:'www.**.com'}，用英文逗号分隔。
 //此处设置的域名和小程序与小程序后台设置的downloadFile合法域名要一致。
 var DOWNLOADFILEDOMAIN = [
-    { id: 1, domain: 'www.watch-life.net' },
+    { id: 1, domain: 'www.lzu.abaoli.cn' },
     { id: 2, domain: 'watch-life.net' },
     { id: 3, domain: 'www.qiniu.com' }
 ]
